17f333635a45ebcb03737b106eae15d42f1546bd
vinicius5581 <viniciussantana@Viniciuss-MacBook-Pro.local>
2018-08-16 12:18:24 -0700

    Users signup, list and delete

--- restAPIs/app.js
+++ restAPIs/app.js
@@ -3,8 +3,10 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+// Routes
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 
 app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
@@ -38,6 +40,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
